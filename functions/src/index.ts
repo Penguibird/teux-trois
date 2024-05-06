44667f31a3c71dced776374ca228322a47d8a209
Vojtech Loskot <loskotaklp@gmail.com>
2024-05-06 10:17:29 +0200

    final

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,23 @@
 import * as functions from "firebase-functions";
-import { DAYINMILIS, getDateId } from "../../src/utils/dateHelpers";
-import { firebaseOptions } from "../../src/services/firebase/firebaseOptions";
+import {DAYINMILIS, getDateId} from "../../src/utils/dateHelpers";
+import {firebaseOptions} from "../../src/services/firebase/firebaseOptions";
 import type Todo from "../../src/types/Todo";
-import { initializeApp } from 'firebase/app'
-import { collection, initializeFirestore, getDocs, doc, writeBatch } from 'firebase/firestore'
+import {initializeApp} from "firebase/app";
+import {
+  collection,
+  initializeFirestore, getDocs, doc, writeBatch,
+} from "firebase/firestore";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
+  functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
 
 
-const app = initializeApp({ ...firebaseOptions });
+const app = initializeApp({...firebaseOptions});
 
 const db = initializeFirestore(app, {});
 
@@ -23,48 +26,48 @@ const db = initializeFirestore(app, {});
 //   .onRequest(async (req, res) => {
 //
 export const moveYesterdaysTodoItems = functions.pubsub
-  .schedule('0 0 3 * * ?')
-  .timeZone('Europe/London')
-  .onRun(async (ctx) => {
-    const currentDate = new Date();
-    const yesterday = new Date(currentDate.getTime() - DAYINMILIS);
+    .schedule("0 0 3 * * ?")
+    .timeZone("Europe/London")
+    .onRun(async (ctx) => {
+      const currentDate = new Date();
+      const yesterday = new Date(currentDate.getTime() - DAYINMILIS);
 
-    const sourceID = getDateId(yesterday.getTime());
-    const targetID = getDateId(currentDate.getTime());
-    // const x = await db.listCollections();
-    const batch = writeBatch(db);
-
-
-    const allUsers = await getDocs(collection(db, 'users'))
-    // const tryUser = await db.collection("users").doc("Aaaa").get()
-    const promises = [] as Promise<any>[]
-    allUsers.forEach((_d) => {
-      promises.push((async (d) => {
-        const targetCol = collection(doc(collection(d.ref, 'todos'), targetID), "items")
-        const allTodosSourceCol = collection(doc(collection(d.ref, 'todos'), sourceID), "items")
+      const sourceID = getDateId(yesterday.getTime());
+      const targetID = getDateId(currentDate.getTime());
+      // const x = await db.listCollections();
+      const batch = writeBatch(db);
 
 
-        const allTodos = await getDocs(allTodosSourceCol);
-        // console.log(JSON.stringify(allTodos))
+      const allUsers = await getDocs(collection(db, "users"));
+      // const tryUser = await db.collection("users").doc("Aaaa").get()
+      const promises = [] as Promise<any>[];
+      allUsers.forEach((_d) => {
+        promises.push((async (d) => {
+          const targetCol = collection(
+              doc(collection(d.ref, "todos"), targetID), "items");
+          const allTodosSourceCol = collection(
+              doc(collection(d.ref, "todos"), sourceID), "items");
 
-        functions.logger.log({ targetID, sourceID })
-        allTodos.docs.forEach((_doc) => {
-          const todo = _doc.data() as Todo;
-          functions.logger.log({ todo })
-          if (todo.done) {
-            return;
-          }
+
+          const allTodos = await getDocs(allTodosSourceCol);
+          // console.log(JSON.stringify(allTodos))
+
+          functions.logger.log({targetID, sourceID});
+          allTodos.docs.forEach((_doc) => {
+            const todo = _doc.data() as Todo;
+            functions.logger.log({todo});
+            if (todo.done) {
+              return;
+            }
 
 
-          functions.logger.log({ message: "Moving ", t: todo.text })
-          batch.delete(_doc.ref);
-          batch.set(doc(targetCol, todo.id), todo);
-        });
-      })(_d))
+            functions.logger.log({message: "Moving ", t: todo.text});
+            batch.delete(_doc.ref);
+            batch.set(doc(targetCol, todo.id), todo);
+          });
+        })(_d));
+      });
+
+      await Promise.all(promises);
+      await batch.commit();
     });
-
-    await Promise.all(promises)
-    await batch.commit();
-
-    res?.send("Success");
-  });
